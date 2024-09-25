998cfbcdc313c393ba514ff7508627035d9693bd
hasanthex <hasan18353@gmail.com>
2024-09-25 12:05:49 +0600

    Navigator material page route

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const FirstScreen(),
-        '/second': (context) => const SecondScreen()
-      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Pages'),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("First Screen"),
+        title: Text(widget.title),
       ),
       body: Center(
         child: ElevatedButton(
             onPressed: () {
-              Navigator.pushNamed(context, "/second");
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SecondScreen()));
             },
             child: const Text("go to 2nd screen")),
       ),
